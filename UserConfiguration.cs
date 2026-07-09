3148fc2212afcdbb1cecc8ae7f33f4ccb099a15f
sajindevgroup <s.razzaghi64@gmail.com>
2026-07-10 01:18:59 +0330

    Commit 0018 - Configure User entity

--- UserConfiguration.cs
+++ UserConfiguration.cs
@@ -5,7 +5,7 @@ using NexaCommerce.Modules.Identity.Domain.Entities;
 
 namespace NexaCommerce.Modules.Identity.Infrastructure.Persistence.Configurations;
 
-public sealed class UserConfiguration : IEntityTypeConfiguration<User>
+internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
     {
@@ -13,11 +13,26 @@ public sealed class UserConfiguration : IEntityTypeConfiguration<User>
 
         builder.HasKey(x => x.Id);
 
+        builder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
         builder.Property(x => x.Email)
-            .HasMaxLength(200)
+            .HasMaxLength(256)
             .IsRequired();
 
         builder.HasIndex(x => x.Email)
             .IsUnique();
+
+        builder.Property(x => x.FirstName)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(x => x.LastName)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(x => x.PasswordHash)
+            .HasMaxLength(500)
+            .IsRequired();
     }
 }
